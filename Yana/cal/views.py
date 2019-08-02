01a4b8a6d338cf4244bef16e652bedad1aeba4a1
mjnathachandrappa@gmail.com <mjnathachandrappa@gmail.com>
2019-08-02 16:40:09 +0530

    adding two numbers using add function

--- Yana/cal/views.py
+++ Yana/cal/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'home.html', {'name': 'manju'})
+    return render(request, 'home.html', {'name': 'Rekhu'})
+
+def add(request):
+    val1 = int(request.GET['num1'])
+    val2 = int(request.GET['num2'])
+    res = val1 + val2
+    return render(request, 'Result.html', {'result':res})
 
